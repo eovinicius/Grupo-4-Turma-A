94e85b1fcfa7053320702e4bc49862723ae5df21
Gustavo Patriota <gpspatriota@gmail.com>
2024-10-14 00:25:40 -0300

    Estruturação Codigo

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,38 @@
 public class Game {
+    public void Iniciar(){
+        //Interface com Menus de Jogar, Créditos, Instruções e Sair
+    }
 
+    public void Jogar(){
+        
+    }
+
+    public void InterfaceCreditos(){
+
+    }
+
+    public void InterfaceInstrucoes(){
+
+    }
+
+    public void PerguntasArray(){
+
+    }
+
+    public void PerguntasCondicionais(){
+
+    }
+
+    public void PerguntasLoops(){
+
+    }
+
+    public void LimpaConsole(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public void ThreadSleep(){
+
+    }
 }
