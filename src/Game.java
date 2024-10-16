85a1d035d17d570bed3322f157224e3a7800cece
Vinicius Oliveira <eovinicius10@gmail.com>
2024-10-15 21:44:06 -0300

    .

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Game {
     public void iniciar() throws InterruptedException {
@@ -198,10 +197,6 @@ public class Game {
     public void LimpaConsole() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-    }
-
-    public void ThreadSleep() {
-
     }
 
     public void exibirMensagem(String mensagem) throws InterruptedException {
