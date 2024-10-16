5c7812d543c660abe9b46c5bfa4becd3ffe2ca88
Gustavo Patriota <gpspatriota@gmail.com>
2024-10-16 15:54:02 -0300

    Try Catch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         Game game = new Game();
-        game.iniciar();
+        try {
+            game.iniciar();
+        } catch (Exception e) {
+            System.out.println("Erro inesperado, reiniciando o jogo...");
+            game.iniciar();
+        }
+        
     }
 }
